c819fdbe344b62e298aae7147ac21c8e8c085fa4
Roman Berezhnoi <berezhnoi.f5@gmail.com>
2016-06-17 08:55:00 +0000

    case page nested views

--- app/js/app.js
+++ app/js/app.js
@@ -1,29 +1,33 @@
 'use strict';
+angular.module('dsaArkTheme', ['ui.router', 'ngResource', 'mm.foundation'])
 
-angular.module( 'dsaArkTheme', ['ui.router', 'ngResource', 'mm.foundation'] )
 
-
-.config( function( $stateProvider, $urlRouterProvider){
-	$urlRouterProvider.otherwise('/');
-	$stateProvider
-		.state( 'home', {
-			url: '/',
-			controller: 'HomeCtrl',
-			templateUrl: appInfo.template_directory + 'app/views/home.html'
-		})
-		.state( 'blog', {
-			url: '/posts?:categories',
-			controller: 'BlogCtrl',
-			templateUrl: appInfo.template_directory + 'app/views/blog.html'
-		})
-		.state( 'single-post', {
-			url: '/posts/:id',
-			controller: 'SinglePostCtrl',
-			templateUrl: appInfo.template_directory + 'app/views/single-post.html'
-		})
-		.state('category', {
-			url: '/categories/:category',
-			controller: 'BlogCtrl',
-			templateUrl: appInfo.template_directory + 'app/views/blog.html'
-		});
+.config(function($stateProvider, $urlRouterProvider) {
+			$urlRouterProvider.otherwise('/');
+			$stateProvider
+				.state('home', {
+					url: '/',
+					controller: 'HomeCtrl',
+					templateUrl: appInfo.template_directory + 'app/views/home.html'
+				})
+				.state('blog', {
+					url: '/posts?:categories',
+					controller: 'BlogCtrl',
+					templateUrl: appInfo.template_directory + 'app/views/blog.html'
+				})
+				.state('single-post', {
+					url: '/posts/:id',
+					controller: 'SinglePostCtrl',
+					templateUrl: appInfo.template_directory + 'app/views/single-post.html'
+				})
+				.state('category', {
+					url: '/categories',
+					controller: 'BlogCtrl',
+					templateUrl: appInfo.template_directory + 'app/views/blog.html'
+				})
+			    .state('category.list', {
+			        url: '/:category',
+			        controller: 'BlogCtrl',
+			        templateUrl: appInfo.template_directory + 'app/views/case-filter.html'
+			    });
 });
